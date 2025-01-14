20c2f78dc256bd72d62b4d30a25ea1ceeb1b9611
Toha Hossain <tohahpro@gmail.com>
2025-01-14 19:02:16 +0600

    Task Details database table create

--- tasks/models.py
+++ tasks/models.py
@@ -8,3 +8,16 @@ class Task(models.Model):
     is_completed = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class TaskDetails(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH, 'High'),
+        (MEDIUM , 'Medium'),
+        (LOW, 'Low'),
+    )
+    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices= PRIORITY_OPTIONS, default=LOW)
