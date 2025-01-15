47698280d3415e6150ca2673e912bd7933527802
Toha Hossain <tohahpro@gmail.com>
2025-01-15 20:42:37 +0600

    many-to-one relationship query setup

--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
+    project= models.ForeignKey(
+        "Projects",
+        on_delete=models.CASCADE, 
+        default=1
+    )
+    # notun_string = models.CharField(max_length=100,default="")
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
@@ -21,3 +27,7 @@ class TaskDetails(models.Model):
     task = models.OneToOneField(Task, on_delete=models.CASCADE)
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices= PRIORITY_OPTIONS, default=LOW)
+
+class Projects(models.Model):
+    name= models.CharField(max_length=100)
+    start_date = models.DateField()
