db6601eeb9f0098ec121194f94f02cfc90916fd8
Toha Hossain <tohahpro@gmail.com>
2025-01-18 11:35:36 +0600

    Use Form Mixins for Clean code

--- tasks/forms.py
+++ tasks/forms.py
@@ -16,22 +16,74 @@ class TaskForm(forms.Form):
         self.fields['assigned_to'].choices = [(emp.id, emp.name) for emp in employees]
 
 
+# Use Form Mixins for Clean Code
+class StyleForMixin:
+    """ Mixin to apply style to form field """
+
+    default_classes = "border border-gray-300 w-full rounded shadow-sm focus:border-rose-500 focus:ring-rose-500"
+
+    def apply_styled_widgets(self):
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'class' : self.default_classes,
+                    'placeholder' : f'Enter {field.label.lower()}'
+                })
+            
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs.update({
+                    'class' : f'{self.default_classes}',
+                    'placeholder' : f'Enter {field.label.lower()}'
+                })
+            elif isinstance(field.widget, forms.SelectDateWidget):
+                field.widget.attrs.update({
+                    'class' : "border border-gray-300 space-y-2 py-1 rounded shadow-sm focus:border-rose-500 focus:ring-rose-500"                    
+                })
+            elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+                field.widget.attrs.update({
+                    'class' : "space-y-2"
+                })
+            else:
+                field.widget.attrs.update({
+                    'class': self.default_classes
+                })
+                
+
+
 # Django Model Form 
-class TaskModelFrom(forms.ModelForm):
+class TaskModelFrom(StyleForMixin,forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description','due_date','assigned_to']
         # exclude = ['project', 'is_completed', 'created_at', 'updated_at']
-
         widgets = {
-            'title':forms.TextInput(attrs={
-                'class':"border border-gray-300 w-full rounded-lg shadow-sm focus:border-rose-500 focus:ring-rose-500", 'placeholder':"Enter Task title"
-            }),
-            'description': forms.Textarea(attrs={
-                'class':"border border-gray-300 w-full rounded-lg shadow-sm focus:border-rose-500 focus:ring-rose-500", 'placeholder':"Enter your description"
-            }),
-            'due_date': forms.SelectDateWidget(attrs={
-                'class':"border border-gray-300 shadow-sm focus:border-rose-500 focus:ring-rose-500"
-            }),
+            'due_date' : forms.SelectDateWidget,
             'assigned_to': forms.CheckboxSelectMultiple
         }
+
+
+# '''Manual widget'''
+        # widgets = {
+        #     'title':forms.TextInput(attrs={
+        #         'class':"border border-gray-300 w-full rounded shadow-sm focus:border-rose-500 focus:ring-rose-500", 'placeholder':"Enter Task title"
+        #     }),
+        #     'description': forms.Textarea(attrs={
+        #         'class':"border border-gray-300 w-full rounded shadow-sm focus:border-rose-500 focus:ring-rose-500", 'placeholder':"Enter your description"
+        #     }),
+        #     'due_date': forms.SelectDateWidget(attrs={
+        #         'class':"border border-gray-300 shadow-sm focus:border-rose-500 focus:ring-rose-500"
+        #     }),
+        #     'assigned_to': forms.CheckboxSelectMultiple
+        # }
+
+# """Widget using mixins"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_styled_widgets()
+
+
+
+    
+     
+    
+
