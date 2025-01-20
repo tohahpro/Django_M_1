58a94079260db825addd7529f73695b62681f9ca
Toha Hossain <tohahpro@gmail.com>
2025-01-20 20:38:19 +0600

    projects status add in database

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 # many to many 
 class Employee(models.Model):
@@ -11,19 +10,27 @@ class Employee(models.Model):
         return self.name
 
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed')
+    ]
     project= models.ForeignKey(
         "Projects",
         on_delete=models.CASCADE, 
         default=1
     )
-    assigned_to = models.ManyToManyField(Employee, related_name='tasks')
-    # notun_string = models.CharField(max_length=100,default="")
+    assigned_to = models.ManyToManyField(Employee, related_name='tasks')    
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='PENDING')
     is_completed = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 class TaskDetails(models.Model):
     HIGH = 'H'
@@ -34,11 +41,19 @@ class TaskDetails(models.Model):
         (MEDIUM , 'Medium'),
         (LOW, 'Low'),
     )
-    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, related_name='details')
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices= PRIORITY_OPTIONS, default=LOW)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'Details form Task {self.task.title}'
 
 class Projects(models.Model):
     name= models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
     start_date = models.DateField()
+
+    def __str__(self):
+        return self.name
 
