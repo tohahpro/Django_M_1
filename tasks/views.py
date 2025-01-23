96f2a1986d47e2b1f24fcda9fc514501cc39358b
Toha Hossain <tohahpro@gmail.com>
2025-01-23 09:27:09 +0600

    Optimizing Database Query

--- tasks/views.py
+++ tasks/views.py
@@ -7,22 +7,20 @@ from datetime import date
 
 # Create your views here.
 
-
 def manager_dashboard(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.select_related('details').prefetch_related('assigned_to').all()    
 
-    # getting task count
-    total_task = tasks.count()
-    completed_task = Task.objects.filter(status='COMPLETED').count()
-    in_progress_task = Task.objects.filter(status='IN_PROGRESS').count()
-    pending_task = Task.objects.filter(status='PENDING').count()
+    # Optimizing Database Query.
+    counts = Task.objects.aggregate(
+        total=Count('id'),
+        completed = Count('id', filter=Q(status = 'COMPLETED')),
+        in_progress = Count('id', filter=Q(status = 'IN_PROGRESS')),
+        pending = Count('id', filter=Q(status = 'PENDING')),
+    )
 
     context = {
         "tasks" : tasks,
-        "total_task" : total_task,
-        "completed_task" : completed_task,
-        "in_progress_task" : in_progress_task,
-        "pending_task" : pending_task
+        "counts" : counts    
     }
     return render(request, "dashboard/manager_dashboard.html",context)
 
