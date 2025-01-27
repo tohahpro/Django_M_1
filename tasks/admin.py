a5efc1dd4e6bd125cc24b45ee3624cd71680355d
Toha Hossain <tohahpro@gmail.com>
2025-01-27 19:51:31 +0600

    django admin signup and admin add

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from tasks.models import Task, TaskDetails, Employee, Projects
 
 # Register your models here.
+
+admin.site.register(Task)
+admin.site.register(TaskDetails)
+admin.site.register(Employee)
+admin.site.register(Projects)
