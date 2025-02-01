5b9146830e9e5e7aee22dee33189593a1139d5b6
Toha Hossain <tohahpro@gmail.com>
2025-02-01 09:37:12 +0600

    Active user setup

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm  
 from django.contrib.auth.models import User
 from users.forms import RegisterFrom ,CustomRegistrationForm 
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from users.forms import LoginForm
+from django.contrib.auth.tokens import default_token_generator
 # Create your views here.
 def sign_up(request):
     if request.method == 'GET':
@@ -49,3 +50,15 @@ def sign_out(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+# USER ACTIVE
+def active_user(request, user_id, token):
+    try:
+        user = User.objects.get(id=user_id)
+        if default_token_generator.check_token(user, token):
+            user.is_active= True
+            user.save()
+            return redirect('sign-in')
+        else:
+            return HttpResponse('Invalid user or id')
+    except User.DoesNotExist:
+        return HttpResponse('User not found')
